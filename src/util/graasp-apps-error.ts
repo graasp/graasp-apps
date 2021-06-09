7af55d59a146a27fd676995a52f7c31c44461deb
André Nogueira <andre.nogueira@epfl.ch>
2021-06-09 18:43:51 +0200

    fix: refactor error codes

--- src/util/graasp-apps-error.ts
+++ src/util/graasp-apps-error.ts
@@ -20,42 +20,42 @@ export class GraaspAppsError implements GraaspError {
 
 export class ItemNotFound extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR001', statusCode: 404, message: 'Item not found' }, data);
+    super({ code: 'GAERR001', statusCode: 404, message: 'Item not found' }, data);
   }
 }
 
 export class NotAnAppItem extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR002', statusCode: 400, message: 'Targeted item is not an application item' }, data);
+    super({ code: 'GAERR002', statusCode: 400, message: 'Targeted item is not an application item' }, data);
   }
 }
 
 export class InvalidApplicationOrigin extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR003', statusCode: 403, message: 'Invalid application, origin pair' }, data);
+    super({ code: 'GAERR003', statusCode: 403, message: 'Invalid application, origin pair' }, data);
   }
 }
 
 export class MemberCannotReadItem extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR004', statusCode: 403, message: 'Member cannot read item' }, data);
+    super({ code: 'GAERR004', statusCode: 403, message: 'Member cannot read item' }, data);
   }
 }
 
 export class TokenItemIdMismatch extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR005', statusCode: 401, message: 'Auth token does not match targeted item' }, data);
+    super({ code: 'GAERR005', statusCode: 401, message: 'Auth token does not match targeted item' }, data);
   }
 }
 
 export class AppDataNotFound extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR006', statusCode: 404, message: 'App data not found' }, data);
+    super({ code: 'GAERR006', statusCode: 404, message: 'App data not found' }, data);
   }
 }
 
 export class AppDataNotAccessible extends GraaspAppsError {
   constructor(data?: unknown) {
-    super({ code: 'GADERR007', statusCode: 403, message: 'Member cannot request this app data' }, data);
+    super({ code: 'GAERR007', statusCode: 403, message: 'Member cannot request this app data' }, data);
   }
 }
