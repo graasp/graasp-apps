a3ef08fa2c97fd9c85a48165b3d139a71e0281d0
pyphilia <pyphilia@gmail.com>
2022-02-07 11:59:24 +0100

    feat: add patch item settings endpoint

---
+++ src/util/utils.ts
@@ -0,0 +1,5 @@
+import { TokenItemIdMismatch } from './graasp-apps-error';
+
+export const checkTargetItemAndTokenItemMatch = (itemId1: string, itemId2: string): void => {
+  if (itemId1 !== itemId2) throw new TokenItemIdMismatch();
+};
